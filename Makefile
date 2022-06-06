4cd209650d10f291b2444caaf83b45bff1adb58d
Malik Alayli <malik.alayli@gmail.com>
2022-06-06 18:16:49 +0200

    feat: play whatislove.it

---
+++ Makefile
@@ -0,0 +1,28 @@
+ifeq ($(strip $(PVSNESLIB_HOME)),)
+$(error "Please create an environment variable PVSNESLIB_HOME with path to its folder and restart application. (you can do it on windows with <setx PVSNESLIB_HOME "/c/snesdev">)")
+endif
+
+AUDIOFILES := whatislove.it
+export SOUNDBANK := soundbank
+
+include ${PVSNESLIB_HOME}/devkitsnes/snes_rules
+
+.PHONY: bitmaps all
+
+#---------------------------------------------------------------------------------
+# ROMNAME is used in snes_rules file
+export ROMNAME := snesSPC
+
+SMCONVFLAGS	:= -s -o $(SOUNDBANK) -v -b 5
+musics: $(SOUNDBANK).obj
+
+all: musics bitmaps $(ROMNAME).sfc
+
+clean: cleanBuildRes cleanRom cleanGfx cleanAudio
+	
+#---------------------------------------------------------------------------------
+pvsneslibfont.pic: pvsneslibfont.bmp
+	@echo convert font with no tile reduction ... $(notdir $@)
+	$(GFXCONV) -n -gs8 -po2 -pc16 -pe1 -mR! -m! -p! $<
+
+bitmaps : pvsneslibfont.pic
---
+++ src/main.c
@@ -0,0 +1,52 @@
+#include <snes.h>
+#include "soundbank.h"
+
+extern char snesfont;
+
+// soundbank that are declared in soundbank.asm
+extern char SOUNDBANK__0,SOUNDBANK__1;
+
+//---------------------------------------------------------------------------------
+int main(void) {
+	// Initialize sound engine (take some time)
+    spcBoot();
+
+    // Initialize SNES 
+	consoleInit();
+
+	// Set give soundbank
+    spcSetBank(&SOUNDBANK__1);
+    spcSetBank(&SOUNDBANK__0);
+
+    // allocate around 10K of sound ram (39 256-byte blocks)
+    spcAllocateSoundRegion(39);
+
+    // Load music
+    spcLoad(MOD_WHATISLOVE);
+    
+    // Initialize text console with our font
+	consoleInitText(0, 0, &snesfont);
+
+	// Now Put in 16 color mode and disable Bgs except current
+	setMode(BG_MODE1,0);
+	bgSetDisable(1);
+	bgSetDisable(2);
+
+	// Draw a wonderfull text :P
+	consoleDrawText(7, 10, "Hello World!");
+	consoleDrawText(7, 14, "WELCOME TO SPC!");
+
+	// Wait for nothing :P
+	setScreenOn();
+
+	// Play file from the beginning
+	spcPlay(0);
+	
+	while(1) {
+		WaitForVBlank();
+
+		// Update music / sfx stream and wait vbl
+        spcProcess();
+	}
+	return 0;
+}
