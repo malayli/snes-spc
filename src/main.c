aa345086f78ca9c0a6094e36c67b1c793bf0a32a
Malik Alayli <malik.alayli@gmail.com>
2022-06-06 18:27:09 +0200

    lint fix

--- src/main.c
+++ src/main.c
@@ -8,13 +8,13 @@ extern char SOUNDBANK__0,SOUNDBANK__1;
 
 //---------------------------------------------------------------------------------
 int main(void) {
-	// Initialize sound engine (take some time)
+    // Initialize sound engine (take some time)
     spcBoot();
 
-    // Initialize SNES 
-	consoleInit();
+    // Initialize SNES
+    consoleInit();
 
-	// Set give soundbank
+    // Set give soundbank
     spcSetBank(&SOUNDBANK__1);
     spcSetBank(&SOUNDBANK__0);
 
@@ -23,30 +23,31 @@ int main(void) {
 
     // Load music
     spcLoad(0);
-    
+
     // Initialize text console with our font
-	consoleInitText(0, 0, &snesfont);
+    consoleInitText(0, 0, &snesfont);
 
-	// Now Put in 16 color mode and disable Bgs except current
-	setMode(BG_MODE1,0);
-	bgSetDisable(1);
-	bgSetDisable(2);
+    // Now Put in 16 color mode and disable Bgs except current
+    setMode(BG_MODE1,0);
+    bgSetDisable(1);
+    bgSetDisable(2);
 
-	// Draw a wonderfull text :P
-	consoleDrawText(7, 10, "Hello World!");
-	consoleDrawText(7, 14, "WELCOME TO SPC!");
+    // Draw a wonderfull text :P
+    consoleDrawText(7, 10, "Hello World!");
+    consoleDrawText(7, 14, "WELCOME TO SPC!");
 
-	// Wait for nothing :P
-	setScreenOn();
+    // Wait for nothing :P
+    setScreenOn();
 
-	// Play file from the beginning
-	spcPlay(0);
-	
-	while(1) {
-		WaitForVBlank();
+    // Play file from the beginning
+    spcPlay(0);
 
-		// Update music / sfx stream and wait vbl
+    while(1) {
+        WaitForVBlank();
+
+        // Update music / sfx stream and wait vbl
         spcProcess();
-	}
-	return 0;
+    }
+    return 0;
 }
+
